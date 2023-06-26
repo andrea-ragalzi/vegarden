6032f4ad59eacf127cfafdfee97f42801b745ca9
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-26 23:13:03 +0200

    created springboot project

---
+++ backend/src/test/java/com/vegarden/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vegarden.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
