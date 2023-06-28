57ef15b0a484f8cec0b6733bba4443f4a84fa6d6
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-29 00:06:16 +0200

    fixed mod route

--- backend/src/main/java/com/vegarden/backend/controllers/TestController.java
+++ backend/src/main/java/com/vegarden/backend/controllers/TestController.java
@@ -32,7 +32,7 @@ public class TestController {
 	}
 
 	@GetMapping("/mod")
-	@PreAuthorize("hasRole('MODERATOR')")
+	@PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
 	public String moderatorAccess() {
 		return "Moderator Board.";
 	}
