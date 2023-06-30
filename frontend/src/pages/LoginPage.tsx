1e0f1a076e428b4d1af821ac8c64503f893d69c6
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-30 12:39:58 +0200

    test palette

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+function LoginPage() {
+    return (
+        <div className="bg-primary-dark h-screen">
+            <h1 className="text-primary">Titolo</h1>
+            <p className="text-secondary">Questo è un testo di esempio.</p>
+            <button className="bg-primary text-white">Pulsante</button>
+        </div>
+    );
+}
+
+
+export default LoginPage;
