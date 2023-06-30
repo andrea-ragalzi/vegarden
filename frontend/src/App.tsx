575262372a403891093bb613ef8d95aa4c4b54ee
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-30 16:57:27 +0200

    added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage'
 
 function App() {
 
   return (
     <div className="App">
-      <LoginPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
