88a56ea74b7b7a0daf0ac824c94ad3b6cd187ea5
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-30 19:31:18 +0200

    added redux provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,13 @@
+import { Provider } from 'react-redux';
+import { store } from './redux/store/store';
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/main.scss'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
-)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement);
+root.render(<Provider store={store}> <App /> </Provider>);
+
+
+
+
