d056c5da6d8e5780a2789789a4eabc48f5d5f2e9
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-30 16:34:00 +0200

    test light and dark mode

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,9 +1,9 @@
 function LoginPage() {
     return (
-        <div className="bg-primary-dark h-screen">
-            <h1 className="text-primary">Titolo</h1>
+        <div className="bg-light dark:bg-dark h-screen">
+            <h1 className="text-primary dark:text-primary">Titolo</h1>
             <p className="text-secondary">Questo è un testo di esempio.</p>
-            <button className="bg-primary text-white">Pulsante</button>
+            <button className="bg-secondary text-white dark:text-dark">Pulsante</button>
         </div>
     );
 }
