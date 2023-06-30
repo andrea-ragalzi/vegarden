9a67c3d9d08cd0b980690a702f65fcf28dc00d86
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-30 19:30:56 +0200

    added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage'
 
-function App() {
+const App = () => {
 
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="App bg-white dark:bg-dark">
+      <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
     </div>
   )
 }
