575954ad721397bf7363d6890fa742e5b454848d
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-07-03 11:14:16 +0200

    replace tailwindcss with bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,12 @@ import LoginPage from './pages/LoginPage'
 const App = () => {
 
   return (
-    <div className="App bg-white dark:bg-dark">
-      <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8">
+    <div className="App container">
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<LoginPage />} />
             </Routes>
           </BrowserRouter>
-        </div>
-      </div>
     </div>
   )
 }
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,27 +2,7 @@ import './../styles/main.scss'
 const LoginPage = () => {
     return (
         <div>
-            <h1 className="text-2xl text-dark dark:text-light">Welcome back to Vegarden!</h1>
-            <h2 className="text-xl text-dark dark:text-light"> Log in and let's continue to make the vegan garden bloom together.</h2>
-            <div className="flex items-center justify-between ">
-                <div className="flex items-center">
-                    <input
-                        id="remember-me"
-                        name="remember-me"
-                        type="checkbox"
-                        className="h-4 w-4 text-purple-600 focus:ring-purple-500 border-gray-300 rounded"
-                    />
-                    <label htmlFor="remember-me" className="ml-2 block text-sm text-purple-900">
-                        Remember me
-                    </label>
-                </div>
-
-                <div className="text-sm">
-                    <a href="#" className="font-medium text-red-900 hover:text-yellow-100">
-                        Forgot your password?
-                    </a>
-                </div>
-            </div>
+            <h1 className='text-primary'>Login</h1>
         </div>
     );
 }
