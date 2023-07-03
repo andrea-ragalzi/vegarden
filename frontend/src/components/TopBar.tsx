bc3011b27086b9a0b30d713db6a6a5673590690f
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-07-04 00:29:20 +0200

    fixed links

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -1,18 +1,22 @@
 import { Row, Col, Button } from 'react-bootstrap';
 import { PersonCircleOutline, FilterOutline, NotificationsOutline } from 'react-ionicons';
+import { Link } from 'react-router-dom';
 
 const TopBar = () => {
     return (
         <Row className='fixed-top row row-cols-2 g-0 justify-content-around bg-primary'>
             <Col className='d-flex justify-content-center'>
                 <Button className="d-flex align-items-center">
-                    <PersonCircleOutline
-                        color={'#000000'}
-                        height="35px"
-                        width={'35px'}
-                        onClick={() => alert('Hi!')}
-                    />
-                    <span className="vegarden-text text-black ms-3">Vegarden</span>
+                    <Link to="/zenhub">
+                        <PersonCircleOutline
+                            color={'#000000'}
+                            height="35px"
+                            width={'35px'}
+                        />
+                    </Link>
+                    <Link to="/home">
+                        <span className="vegarden-text text-black ms-3">Vegarden</span>
+                    </Link>
                 </Button>
             </Col>
             <Col className='d-flex justify-content-end'>
