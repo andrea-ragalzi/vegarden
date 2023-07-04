0ef6abc46ec5c125a68910037e287ecdf71f644e
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-07-04 14:05:07 +0200

    fixed store path

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux';
-import { store } from './redux/store/store';
+import { store } from './store/store.ts';
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/main.scss'
