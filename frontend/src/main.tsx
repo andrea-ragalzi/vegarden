0d793b6f586b23031f5fcab915a523e5afda1e6d
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-07-05 14:40:01 +0200

    implemented persistance

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,17 @@
 import { Provider } from 'react-redux';
-import { store } from './store/store.ts';
+import { store, persistor } from './store/store.ts';
+import { PersistGate } from "redux-persist/integration/react";
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/main.scss'
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement);
-root.render(<Provider store={store}> <App /> </Provider>);
 
-
-
-
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -2,15 +2,26 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import loginReducer from "../reducers/loginReducer";
 import zenyteReducer from "../reducers/zenyteReducer";
-
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const allReducers = combineReducers({
   login: loginReducer,
   zenyte: zenyteReducer
 });
 
+const persistConfig = {
+  key: "root",
+  storage,
+  // Puoi specificare qui il whitelist o il blacklist dei reducer
+};
+
+const persistedReducer = persistReducer(persistConfig, allReducers);
+
 export const store = configureStore({
-  reducer: allReducers,
+  reducer: persistedReducer
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
