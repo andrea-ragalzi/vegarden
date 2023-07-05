7411764449c2a706c8bf0339da666030ceff5354
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-07-05 14:34:10 +0200

    added zenyte reducer

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
+import loginReducer from "../reducers/loginReducer";
+import zenyteReducer from "../reducers/zenyteReducer";
 
 
 const allReducers = combineReducers({
-
+  login: loginReducer,
+  zenyte: zenyteReducer
 });
 
 export const store = configureStore({
