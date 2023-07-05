f117d8a355c84659ae3cb1fa8e3cea48aa806365
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-07-05 19:54:38 +0200

    dynamic profiel data

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,14 +1,17 @@
 import { Row, Col, Image } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
 
 const Profile = () => {
+    const profile = useSelector((state: RootState) => state.profile.profile);
     return (
         <>
             <Row className='justify-content-center gx-0 mx-2 text-black'>
                 <Col xs={8}>
                     <Image className='rounded-circle mb-1' src="https://picsum.photos/120/120" alt="Avatar"></Image>
-                    <p className='mb-0 ms-2'>Andrea Ragalzi</p>
-                    <p className='mb-0 ms-2'>@andreaseperso</p>
-                    <p className='ms-2'>He/Him</p>
+                    <p className='mb-0 ms-2'>{profile?.firstname + ' ' + profile?.lastname}</p>
+                    <p className='mb-0 ms-2'>@{profile?.owner.username}</p>
+                    <p className='ms-2'>{profile?.pronouns}</p>
                 </Col>
                 <Col xs={4} className='mt-4'>
                     <p>10 Article</p>
@@ -18,9 +21,7 @@ const Profile = () => {
             </Row>
             <Row className='text-black'>
                 <Col>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Nam eos rem veritatis, facere, officia expedita in suscipit eius minima corporis maxime cumque
-                        quos deleniti quae et excepturi architecto tempore pariatur?</p>
+                    <p>{profile?.bio}</p>
                 </Col>
             </Row>
         </>
