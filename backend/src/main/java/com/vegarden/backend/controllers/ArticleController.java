014d58017d3f07ca2e025673e203a5036f6b51d6
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-07-13 16:48:51 +0200

    added coverImage

--- backend/src/main/java/com/vegarden/backend/controllers/ArticleController.java
+++ backend/src/main/java/com/vegarden/backend/controllers/ArticleController.java
@@ -1,6 +1,7 @@
 package com.vegarden.backend.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.vegarden.backend.models.Article;
 import com.vegarden.backend.models.Blog;
@@ -8,15 +9,31 @@ import com.vegarden.backend.services.ArticleService;
 import com.vegarden.backend.services.BlogService;
 import com.vegarden.backend.services.ZenyteService;
 
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.ResourceUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,9 +42,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RequestMapping("/api/articles")
 public class ArticleController {
 
@@ -58,28 +76,48 @@ public class ArticleController {
         }
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<Article> postArticle(
-            @RequestBody Article newArticle,
+            @RequestPart("title") String title,
+            @RequestPart("description") String description,
+            @RequestPart("body") String body,
+            @RequestPart("coverImage") MultipartFile coverImageFile,
             @AuthenticationPrincipal UserDetails userDetails) {
         if (userDetails == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
-        Timestamp now = new Timestamp(System.currentTimeMillis());
-        Blog blog = blogService.findBlogByOwner(
-                zenyteService.findZenyteByUsername(userDetails.getUsername()));
-        Article article = new Article();
-        article.setTitle(newArticle.getTitle());
-        article.setDescription(newArticle.getDescription());
-        article.setBody(newArticle.getBody());
-        article.setBlog(blog);
-        // TO DO: add body html
-        // article.setBodyHtml(newArticle.getBodyHtml());
-        article.setCreatedAt(now);
-        articleService.saveArticle(article);
-        return ResponseEntity.status(HttpStatus.CREATED).body(article);
+        try {
+            String uploadLocation = "/home/andrea/Workspace/vegarden/backend/src/main/resources/uploads/cover_images";
+
+            // Genera un nome univoco per il file dell'immagine di copertina
+            String coverImageFileName = UUID.randomUUID().toString() + "-" + coverImageFile.getOriginalFilename();
+
+            // Crea il percorso completo del file dell'immagine
+            String coverImageFilePath = uploadLocation + File.separator + coverImageFileName;
+
+            // Copia l'immagine sul disco
+            Files.copy(coverImageFile.getInputStream(), Path.of(coverImageFilePath),
+                    StandardCopyOption.REPLACE_EXISTING);
+
+            Timestamp now = new Timestamp(System.currentTimeMillis());
+            Blog blog = blogService.findBlogByOwner(
+                    zenyteService.findZenyteByUsername(userDetails.getUsername()));
+            Article article = new Article();
+            article.setTitle(title);
+            article.setCoverImageURL(coverImageFilePath);
+            article.setDescription(description);
+            article.setBody(body);
+            article.setBlog(blog);
+            article.setCreatedAt(now);
+            articleService.saveArticle(article);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(article);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PutMapping("/{id}")
