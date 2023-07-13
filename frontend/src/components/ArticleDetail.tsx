d824e3c3f92f1cb3bf34b13c171d49113e87ce35
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-07-13 16:52:29 +0200

    dynamic images

--- frontend/src/components/ArticleDetail.tsx
+++ frontend/src/components/ArticleDetail.tsx
@@ -1,72 +1,128 @@
 import { Col, Row, Image, Button } from "react-bootstrap";
-import { getArticle } from "../actions/articleAction";
 import { Article } from "../types/articleType";
+import { BookmarkOutline, ChatbubblesOutline, FlowerOutline, ShareSocialOutline } from "react-ionicons";
 import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { RootState, store } from "../store/store";
-import { useNavigate, useParams } from "react-router-dom";
-import { AddOutline, BookmarkOutline, ChatbubbleOutline, ChatbubblesOutline, ChatbubblesSharp, FlowerOutline, HomeOutline, SearchOutline, ShareOutline, ShareSocialOutline } from "react-ionicons";
+import { RootState } from "../store/store";
 
 const ArticleDetail = ({ article }: { article: Article }) => {
+    const navigate = useNavigate();
+    const loggedIn = useSelector((state: RootState) => state.login.loggedIn);
+    const currentRoute = useLocation().pathname;
+    const [coverImageURL, setCoverImageURL] = useState<string | undefined>(undefined);
+
+    useEffect(() => {
+        const getFileNameFromBlobURL = (blobURL: string) => {
+            const splitURL = blobURL.split('/');
+            return splitURL[splitURL.length - 1];
+        };
+        if (!loggedIn) {
+            navigate('/');
+        }
+        console.log('DETAIL');
+        console.log(article);
+        if (currentRoute !== "/article-create") {
+            fetchCoverImage(getFileNameFromBlobURL(article.coverImageURL!));
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [article, loggedIn]);
+
+    const fetchCoverImage = async (filename: string) => {
+        console.log(filename);
+        try {
+            const response = await fetch(`http://localhost:8080/api/uploads/cover_images/${filename}`);
+            if (response.ok) {
+                const imageBlob = await response.blob();
+                const imageURL = URL.createObjectURL(imageBlob);
+                setCoverImageURL(imageURL);
+                console.log(imageURL);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <>
             <Row className='row-cols-1 justify-content-center align-items-center article-detail text-center'>
-                <Col>
-                    <h1>{article?.title}</h1>
-                </Col>
-                <Col>
-                    <p>{article?.createdAt}</p>
-                </Col>
-                <Col>
-                    <Image src={article?.coverImage || "https://picsum.photos/300/300"} alt={article?.title} />
-                </Col>
-                <Col>
-                    <p>{article?.body}</p>
-                </Col>
-                <Col>
-                    <p>{article?.collaborators.map((collaborator) => collaborator.username).join(", ")}</p>
-                </Col>
+                {article && (
+                    <>
+                        <Col>
+                            <h1>{article?.title}</h1>
+                        </Col>
+                        <Col>
+                            <p>{article?.createdAt}</p>
+                        </Col>
+                        {article?.coverImageURL && (
+                            <Col>
+                                <Image
+                                    className="cover"
+                                    src={coverImageURL}
+                                    alt={article.title}
+                                />
+                            </Col>
+                        )}
+                        {article?.coverImage && currentRoute === "/article-create" && (
+                            <Col>
+                                <Image
+                                    className="cover"
+                                    src={URL.createObjectURL(article.coverImage)}
+                                    alt={article.title}
+                                />
+                            </Col>
+                        )}
+                        <Col>
+                            <p className="body">{article?.body}</p>
+                        </Col>
+                        <Col>
+                            <p>{article?.collaborators.map((collaborator) => collaborator.username).join(", ")}</p>
+                        </Col>
+                    </>
+                )}
             </Row>
-            <Row>
-                <Col className='d-flex justify-content-center'>
-                    <Button className="reaction">
-                        <FlowerOutline
-                            height="35px"
-                            width={'35px'}
-                            onClick={() => alert('Work progress!')}
-                        />
-                    </Button>
-                </Col>
-                <Col className='d-flex justify-content-center'>
-                <Button className="reaction">
-                        <ChatbubblesOutline
-                            color={'#000000'}
-                            height="35px"
-                            width={'35px'}
-                            onClick={() => alert('Work progress!')}
-                        />
-                    </Button>
-                </Col>
-                <Col className='d-flex justify-content-center'>
-                <Button className="reaction">
-                        <BookmarkOutline
-                            color={'#000000'}
-                            height="35px"
-                            width={'35px'}
-                            onClick={() => alert('Work in progress!')}
-                        />
-                    </Button>
-                </Col>
-                <Col className='d-flex justify-content-center'>
-                <Button className="reaction">
-                        <ShareSocialOutline
-                            height="35px"
-                            width={'35px'}
-                            onClick={() => alert('Work in progress!')}
-                        />
-                    </Button>
-                </Col>
-            </Row>
+            {currentRoute !== "/article-create" && (
+                <Row>
+                    <Col className='d-flex justify-content-center'>
+                        <Button className="reaction">
+                            <FlowerOutline
+                                height="35px"
+                                width={'35px'}
+                                onClick={() => alert('Work progress!')}
+                            />
+                        </Button>
+                    </Col>
+                    <Col className='d-flex justify-content-center'>
+                        <Button className="reaction">
+                            <ChatbubblesOutline
+                                color={'#000000'}
+                                height="35px"
+                                width={'35px'}
+                                onClick={() => alert('Work progress!')}
+                            />
+                        </Button>
+                    </Col>
+                    <Col className='d-flex justify-content-center'>
+                        <Button className="reaction">
+                            <BookmarkOutline
+                                color={'#000000'}
+                                height="35px"
+                                width={'35px'}
+                                onClick={() => alert('Work in progress!')}
+                            />
+                        </Button>
+                    </Col>
+                    <Col className='d-flex justify-content-center'>
+                        <Button className="reaction">
+                            <ShareSocialOutline
+                                height="35px"
+                                width={'35px'}
+                                onClick={() => alert('Work in progress!')}
+                            />
+                        </Button>
+                    </Col>
+                </Row>
+            )}
         </>
     );
 }
--- frontend/src/components/ArticleMaker.tsx
+++ frontend/src/components/ArticleMaker.tsx
@@ -1,10 +1,12 @@
-import { ChangeEvent, FormEvent, useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
 import { useSelector } from "react-redux";
-import { createArticle } from "../actions/articleAction";
+import { createArticle, setArticle } from "../actions/articleAction";
 import { RootState, store } from "../store/store";
 import { Article } from "../types/articleType";
 import { useNavigate } from "react-router-dom";
+import { readBlog } from "../actions/blogAction";
+import ArticleDetail from "./ArticleDetail";
 
 const ArticleMaker = () => {
     const navigate = useNavigate();
@@ -13,8 +15,29 @@ const ArticleMaker = () => {
     const [description, setDescription] = useState("");
     const [body, setBody] = useState("");
     const [coverImage, setCoverImage] = useState<File | null>(null);
-    const accessToken = useSelector((state: RootState) => state.login.session.accessToken);
+    const { session, loggedIn } = useSelector((state: RootState) => state.login);
     const blog = useSelector((state: RootState) => state.blog.blog);
+    const formData = new FormData();
+    const article: Article = {
+        id: 0,
+        title: title,
+        coverImage: coverImage,
+        description: description,
+        body: body,
+        createdAt: '',
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        blog: blog!,
+        collaborators: []
+    };
+
+    useEffect(() => {
+        formData.append("title", title);
+        formData.append("description", description);
+        formData.append("body", body);
+        if (coverImage) {
+            formData.append("coverImage", coverImage);
+        }
+    }, [title, body, description, coverImage]);
 
     const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.files && e.target.files.length > 0) {
@@ -24,86 +47,73 @@ const ArticleMaker = () => {
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
- 
-        console.log('Posting article');
-        const formData = new FormData();
-        formData.append("title", title);
-        formData.append("description", description);
-        formData.append("body", body);
-        if (coverImage) {
-            formData.append("coverImage", coverImage);
+        if (title && description && body) {
+            dispatch(createArticle(formData, session.accessToken,));
+            dispatch(readBlog(session.username, session.accessToken));
+            navigate("/zenhub/me");
         }
-        const article: Article = {
-            id: 0,
-            title: title,
-            description: description,
-            body: body,
-            createdAt: '',
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            blog: blog!,
-            collaborators: []
-        };
-        dispatch(createArticle(accessToken, article));
-        navigate("/zenhub/me");
     };
 
     return (
-        <>
-            <h1 className="mb-3 text-center text-secondary">Write Article</h1>
-            <Form onSubmit={handleSubmit} className="text-center text-dark">
-                <Form.Group controlId="formTitle" className="mb-2">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter title"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        className="my-input"
-                    />
-                </Form.Group>
+        <Row className='justify-content-center mx-5 text-black'>
+            <Col xs={12} md={{ span: 6, order: 1 }} className='mt-5'>
+                <h1 className="mb-3 text-center text-secondary">Write Article</h1>
+                <Form onSubmit={handleSubmit} className="text-center text-dark">
+                    <Form.Group controlId="formTitle" className="mb-2">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter title"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            className="my-input"
+                        />
+                    </Form.Group>
 
-                <Form.Group controlId="formCoverImage" className="mb-2">
-                    <Form.Label>Cover Image</Form.Label>
-                    <Form.Control
-                        type="file"
-                        accept="image/*"
-                        onChange={handleImageChange}
-                        className="my-input"
-                    />
-                </Form.Group>
+                    <Form.Group controlId="formCoverImage" className="mb-2">
+                        <Form.Label>Cover Image</Form.Label>
+                        <Form.Control
+                            type="file"
+                            accept="image/*"
+                            onChange={handleImageChange}
+                            className="my-input"
+                        />
+                    </Form.Group>
 
-                <Form.Group controlId="formDescription" className="mb-2">
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter description"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        className="my-input"
-                    />
-                </Form.Group>
+                    <Form.Group controlId="formDescription" className="mb-2">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            className="my-input"
+                        />
+                    </Form.Group>
 
-                <Form.Group controlId="formBody" className="mb-3">
-                    <Form.Label>Body</Form.Label>
-                    <Form.Control
-                        as="textarea"
-                        rows={5}
-                        placeholder="Enter body"
-                        value={body}
-                        onChange={(e) => setBody(e.target.value)}
-                        className="my-input"
-                    />
-                </Form.Group>
+                    <Form.Group controlId="formBody" className="mb-3">
+                        <Form.Label>Body</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            rows={5}
+                            placeholder="Enter body"
+                            value={body}
+                            onChange={(e) => setBody(e.target.value)}
+                            className="my-input"
+                        />
+                    </Form.Group>
 
-                <Button variant="info" size="lg" type="submit" className="w-100 mb-2">
-                    Preview
-                </Button>
-
-                <Button variant="secondary" size="lg" type="submit" className="w-100">
-                    Publish
-                </Button>
-            </Form>
-        </>
+                    <Button variant="secondary" size="lg" type="submit" className="w-100">
+                        Publish
+                    </Button>
+                </Form>
+            </Col>
+            {(article.title || article.body || article.coverImage) && (
+                <Col xs={12} md={{ span: 6, order: 0 }} className="mt-5">
+                    <ArticleDetail article={article} />
+                </Col>
+            )}
+        </Row>
     );
 };
 
