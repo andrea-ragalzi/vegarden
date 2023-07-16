c32b5ec34d7bb8df938a3e9f4f78a74226159de6
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-07-16 16:08:29 +0200

    added methods

--- backend/src/main/java/com/vegarden/backend/repositories/ArticleReactionRepository.java
+++ backend/src/main/java/com/vegarden/backend/repositories/ArticleReactionRepository.java
@@ -4,9 +4,18 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.vegarden.backend.models.Article;
 import com.vegarden.backend.models.ArticleReaction;
+import com.vegarden.backend.models.Zenyte;
+
+import jakarta.transaction.Transactional;
 
 public interface ArticleReactionRepository extends JpaRepository<ArticleReaction, Long> {
 
     List<ArticleReaction> findByArticleId(Long articleId);
+
+    boolean existsByArticleAndAuthor(Article article, Zenyte author);
+
+    @Transactional
+    void deleteByArticleAndAuthor(Article article, Zenyte author);
 }
